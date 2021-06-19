e9116e31c316d625eb7db6b0d7bf2966ca787acb
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-06-19 17:10:59 +0200

    Optimising _Fractions() class by reducing "expensive" division operations if not needed

--- source/timer.py
+++ source/timer.py
@@ -39,11 +39,11 @@ class Timer:
 	class _Fractions:
 		def __init__(self, elapsed_time):
 			_microseconds, _nanoseconds = divmod(elapsed_time, 1000)
-			_milliseconds, _microseconds = divmod(_microseconds, 1000)
-			_seconds, _milliseconds = divmod(_milliseconds, 1000)
-			_minutes, _seconds = divmod(_seconds, 60)
-			_hours, _minutes = divmod(_minutes, 60)
-			_days, _hours = divmod(_hours, 24)
+			_milliseconds, _microseconds = divmod(_microseconds, 1000) if _microseconds > 0 else (0, 0)
+			_seconds, _milliseconds = divmod(_milliseconds, 1000) if _milliseconds > 0 else (0, 0)
+			_minutes, _seconds = divmod(_seconds, 60) if _seconds > 0 else (0, 0)
+			_hours, _minutes = divmod(_minutes, 60) if _minutes > 0 else (0, 0)
+			_days, _hours = divmod(_hours, 24) if _hours > 0 else (0, 0)
 			self.nanoseconds = int(_nanoseconds)
 			self.microseconds = int(_microseconds)
 			self.milliseconds = int(_milliseconds)
