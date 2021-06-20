f8fd3c71e4bab62946c4a08e9ba64d0dbf247812
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-06-20 20:23:16 +0200

    Fix: seconds_rounded isn't a value and should instead call seconds_rounded() as function

--- src/timer/timer.py
+++ src/timer/timer.py
@@ -37,11 +37,11 @@ class Timer:
 			text_intro = f"Elapsed time{'' if self.is_thread_none(thread) else f' (thread {textcolour.green}{thread}{textcolour.reset})'}:"
 			fractions = TimeFractions(elapsed_time)
 			if fractions.days > 0:
-				print(f"{text_intro} {fractions.days}d {fractions.hours}h {fractions.minutes}m {fractions.seconds_rounded}s") # Format: 1d 2h 3m 4s
+				print(f"{text_intro} {fractions.days}d {fractions.hours}h {fractions.minutes}m {fractions.seconds_rounded()}s") # Format: 1d 2h 3m 4s
 			elif fractions.hours > 0:
-				print(f"{text_intro} {fractions.hours}h {fractions.minutes}m {fractions.seconds_rounded}s") # Format: 1h 2m 3s
+				print(f"{text_intro} {fractions.hours}h {fractions.minutes}m {fractions.seconds_rounded()}s") # Format: 1h 2m 3s
 			elif fractions.minutes > 0:
-				print(f"{text_intro} {fractions.count_seconds_to_float():.{decimals}f} seconds ({fractions.minutes}m {fractions.seconds_rounded}s)") # Format: 1m 2s
+				print(f"{text_intro} {fractions.count_seconds_to_float():.{decimals}f} seconds ({fractions.minutes}m {fractions.seconds_rounded()}s)") # Format: 1m 2s
 			elif fractions.seconds > 0:
 				print(f"{text_intro} {fractions.count_seconds_to_float():.{decimals}f} seconds") # Format: 0.123456789 seconds
 			elif fractions.milliseconds > 0:
