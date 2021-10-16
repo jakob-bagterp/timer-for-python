96a2d8b1c626858549930aa4d09131609e6f654d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-10-17 01:29:45 +0300

    Refactor and optimise get index and item for threads so it happens all at once without extra steps

--- src/timer/controller/__init__.py
+++ src/timer/controller/__init__.py
@@ -9,8 +9,8 @@ def start(timer: object, thread: str, start_time: int, decimals: int) -> None:
     try:
         thread = helper.thread.normalise_to_string_and_uppercase(thread)
         decimals = helper.decimals.mediate(timer, decimals)
-        entry_index = helper.thread.list.lookup_index(timer, thread)
-        if entry_index is None: # If no match in existing threads, create new entry in the thread list.
+        thread_item, _ = helper.thread.list.try_get_thread_item_and_index(timer, thread)
+        if thread_item is None: # If no match in existing threads, create new entry in the thread list.
             helper.thread.list.add(timer, thread, start_time, decimals)
         else:
             error.start_controller(thread)
@@ -20,9 +20,8 @@ def start(timer: object, thread: str, start_time: int, decimals: int) -> None:
 def stop(timer: object, thread: str, stop_time: int) -> None:
     try:
         thread = helper.thread.normalise_to_string_and_uppercase(thread)
-        entry_index = helper.thread.list.lookup_index(timer, thread)
-        if entry_index is not None: # If there's a match in existing threads, return values to the stop function and remove the entry.
-            thread_item = helper.thread.list.get_thread_item(timer, entry_index)
+        thread_item, entry_index = helper.thread.list.try_get_thread_item_and_index(timer, thread)
+        if thread_item is not None: # If there's a match in existing threads, return values to the stop function and remove the entry.
             elapsed_time = stop_time - thread_item.start_time
             helper.thread.list.remove(timer, entry_index)
             helper.output_message(thread, elapsed_time, thread_item.decimals)
