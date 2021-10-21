1d9e03f414957f79c35ebedd2d2aa1905f56a928
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-10-21 17:04:09 +0200

    Refactor countdown into loop function

--- pipeline.sh
+++ pipeline.sh
@@ -11,26 +11,15 @@ pip3 uninstall timer-for-python;
 echo "";
 
 # It takes a moment before the latest version of the newly uploaded package on PyPI is avaible. Let's wait a moment.
-echo "Waiting 10 seconds before reinstall";
-sleep 1s;
-echo "Waiting 9 seconds before reinstall";
-sleep 1s;
-echo "Waiting 8 seconds before reinstall";
-sleep 1s;
-echo "Waiting 7 seconds before reinstall";
-sleep 1s;
-echo "Waiting 6 seconds before reinstall";
-sleep 1s;
-echo "Waiting 5 seconds before reinstall";
-sleep 1s;
-echo "Waiting 4 seconds before reinstall";
-sleep 1s;
-echo "Waiting 3 seconds before reinstall";
-sleep 1s;
-echo "Waiting 2 seconds before reinstall";
-sleep 1s;
-echo "Waiting 1 second before reinstall";
-sleep 1s;
+for i in {10..1}
+    do
+        if [ $i == 1 ]; then
+            echo "Waiting $i second before reinstall..."
+        else
+            echo "Waiting $i seconds before reinstall..."
+        fi
+        sleep 1s;
+    done
 
 pip3 install timer-for-python;
 #python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps timer-for-python;
