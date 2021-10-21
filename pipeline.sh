7d9b0b7083640a35fce0eb1845973a9e657fac3d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-10-21 17:20:08 +0200

    Remove unneeded end-of-line semicolons

--- pipeline.sh
+++ pipeline.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-python3 build_package.py;
-echo "";
+python3 build_package.py
+echo ""
 
-python3 deploy_package.py;
-#python3 deploy_test_package.py;
-echo "";
+python3 deploy_package.py
+#python3 deploy_test_package.py
+echo ""
 
-pip3 uninstall timer-for-python;
-echo "";
+pip3 uninstall timer-for-python
+echo ""
 
 # It takes a moment before the latest version of the newly uploaded package on PyPI is avaible. Let's wait a moment.
 for i in {10..1}; do
@@ -17,8 +17,8 @@ for i in {10..1}; do
     else
         echo "Waiting $i seconds before reinstall..."
     fi
-    sleep 1s;
+    sleep 1s
 done
 
-pip3 install timer-for-python;
-#python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps timer-for-python;
+pip3 install timer-for-python
+#python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps timer-for-python
