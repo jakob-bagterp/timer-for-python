94e7ae368c672009ac23a35d6e6752536c663ae5
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-10-21 15:49:25 +0200

    Add countdown from 10 seconds so the terminal doesn't appear idle while waiting

--- pipeline.sh
+++ pipeline.sh
@@ -8,6 +8,27 @@ echo "";
 pip3 uninstall timer-for-python;
 echo "";
 
-sleep 10s; # It takes a moment before the latest version of the newly uploaded package on PyPI is avaible. Let's wait a moment.
+# It takes a moment before the latest version of the newly uploaded package on PyPI is avaible. Let's wait a moment.
+echo "Waiting 10 seconds before reinstall";
+sleep 1s;
+echo "Waiting 9 seconds before reinstall";
+sleep 1s;
+echo "Waiting 8 seconds before reinstall";
+sleep 1s;
+echo "Waiting 7 seconds before reinstall";
+sleep 1s;
+echo "Waiting 6 seconds before reinstall";
+sleep 1s;
+echo "Waiting 5 seconds before reinstall";
+sleep 1s;
+echo "Waiting 4 seconds before reinstall";
+sleep 1s;
+echo "Waiting 3 seconds before reinstall";
+sleep 1s;
+echo "Waiting 2 seconds before reinstall";
+sleep 1s;
+echo "Waiting 1 second before reinstall";
+sleep 1s;
+
 pip3 install timer-for-python;
 #python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps timer-for-python;
