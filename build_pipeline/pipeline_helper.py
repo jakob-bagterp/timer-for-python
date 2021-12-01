3f737520b2d75772264abe9719042d65bdd82491
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-01 16:19:21 +0100

    Pipeline: Refactor set working directory from halfway static to dynamic using the pathlib library

--- build_pipeline/pipeline_helper.py
+++ build_pipeline/pipeline_helper.py
@@ -1,10 +1,10 @@
 import os
-import re
+from pathlib import Path
 
 def set_working_directory(directory: str) -> None:
     os.chdir(directory)
 
 def set_project_base_path_as_working_directory() -> None:
-    path_of_this_file, _ = os.path.split(os.path.abspath(__file__))
-    project_base_directory = re.sub(r"/build_pipeline\/?$", "", path_of_this_file, flags = re.IGNORECASE) # Assume this is the directory name of the pipeline build files.
+    path_of_this_file = Path(__file__)
+    project_base_directory = path_of_this_file.parent.parent.absolute() # The parent directory of this "build_pipeline".
     set_working_directory(project_base_directory)
