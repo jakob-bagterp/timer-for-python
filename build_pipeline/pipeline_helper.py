9feb0d97d36f6ead842823b9701735e885d220ce
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-01 15:50:27 +0100

    Minor refactor of pipeline helper function to set directory

--- build_pipeline/pipeline_helper.py
+++ build_pipeline/pipeline_helper.py
@@ -1,7 +1,10 @@
 import os
 import re
 
-def set_project_base_path_as_working_directory():
+def set_working_directory(directory: str) -> None:
+    os.chdir(directory)
+
+def set_project_base_path_as_working_directory() -> None:
     path_of_this_file, _ = os.path.split(os.path.abspath(__file__))
     project_base_directory = re.sub(r"/build_pipeline\/?$", "", path_of_this_file, flags = re.IGNORECASE) # Assume this is the directory name of the pipeline build files.
-    os.chdir(project_base_directory)
+    set_working_directory(project_base_directory)
