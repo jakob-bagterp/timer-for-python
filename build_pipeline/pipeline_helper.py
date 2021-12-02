e6a144eabcd4d84fd5e12ce64881da8a79b28cfb
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-02 09:46:24 +0100

    Pipeline: Minor update comments

--- build_pipeline/pipeline_helper.py
+++ build_pipeline/pipeline_helper.py
@@ -6,5 +6,5 @@ def set_working_directory(directory: str) -> None:
 
 def set_project_base_path_as_working_directory() -> None:
     path_of_this_file = Path(__file__)
-    project_base_directory = path_of_this_file.parent.parent.absolute() # The parent directory of this "build_pipeline".
+    project_base_directory = path_of_this_file.parent.parent.absolute() # The parent directory of this "build_pipeline" directory.
     set_working_directory(project_base_directory)
