f387fef4f0f42c290e50dca84c0b4858e3006334
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-02 09:59:54 +0100

    Pipeline: Refactor helpers into separate folder so it's easier to overview

--- build_pipeline/build_package.py
+++ build_pipeline/build_package.py
@@ -1,8 +1,8 @@
 import glob
 import os
 import subprocess
-from pipeline_constants import build_directory
-import pipeline_helper
+from helper.constants import build_directory
+from helper import working_directory
 
 def has_build_directory() -> bool:
     return os.path.exists(f"./{build_directory()}")
@@ -17,6 +17,6 @@ def build_package() -> None:
     subprocess.call("python3 -m build".split())
 
 if __name__ == "__main__":
-    pipeline_helper.set_project_base_path_as_working_directory()
+    working_directory.set_as_project_base_path()
     prune_all_files_from_build_directory() # Remove previous builds since the twine deployment tool can't handle multiple builds.
     build_package()
--- build_pipeline/deploy_test_package.py
+++ build_pipeline/deploy_test_package.py
@@ -1,10 +1,10 @@
 import subprocess
-from pipeline_constants import build_directory
-import pipeline_helper
+from .helper.constants import build_directory
+from .helper import working_directory
 
 def deploy_test_package() -> None:
     subprocess.call(f"python3 -m twine upload --repository testpypi {build_directory()}/*".split())
 
 if __name__ == "__main__":
-    pipeline_helper.set_project_base_path_as_working_directory()
+    working_directory.set_as_project_base_path()
     deploy_test_package()
