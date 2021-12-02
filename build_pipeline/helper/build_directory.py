2f6c3b4473ff554d0c0ce04d2b122291b8e4b0d8
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-02 17:49:08 +0100

    Pipeline: Add working directory guard to build helper so files aren't accidentally deleted

--- build_pipeline/helper/build_directory.py
+++ build_pipeline/helper/build_directory.py
@@ -1,11 +1,14 @@
 import glob
 import os
+from . import working_directory
 from .constants import build_destination
 
 def exists() -> bool:
+    working_directory.set_as_project_base_path()
     return os.path.exists(f"./{build_destination()}")
 
 def prune_old_files() -> None:
+    working_directory.set_as_project_base_path()
     if exists():
         all_build_files = glob.glob(f"./{build_destination()}/*")
         for file in all_build_files:            
