9de02a527a294c9b3f78b63c525b77967f13432a
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-04 15:16:42 +0100

    Test: Refactor imports to avoid naming conflicts and align with updated naming in constants

--- test/helper/decimals/validate_and_normalise_test.py
+++ test/helper/decimals/validate_and_normalise_test.py
@@ -1,19 +1,19 @@
 import numpy
-from timer.constants.decimals import default as default_decimals
-import timer.helper.decimals as decimals
+from timer.constants import decimals
+from timer.helper.decimals import validate_and_normalise
 
 class TestDecimalsValidationAndNormalisation():
     def test_range_0_10(self):
-        for input in range(0, 10):
-            assert decimals.validate_and_normalise(input) == input
+        for input in range(0, decimals.maximum() + 1):
+            assert validate_and_normalise(input) == input
     
     def test_number_larger_than_9_should_default(self):
-        for input in range(10, 100):
-            assert decimals.validate_and_normalise(input) == 9
+        for input in range(decimals.maximum() + 1, 100):
+            assert validate_and_normalise(input) == decimals.maximum()
 
     def test_negative_number_should_default(self):
         for input in range(-1, -100):
-            assert decimals.validate_and_normalise(input) == default_decimals()
+            assert validate_and_normalise(input) == decimals.default()
     
     def test_float_should_default(self):
         assert decimals.validate_and_normalise(3.5) == 2
@@ -21,9 +21,9 @@ class TestDecimalsValidationAndNormalisation():
 
     def test_negative_float_should_default(self):
         for input in numpy.arange(-100, 0, 0.1):
-            assert decimals.validate_and_normalise(input) == default_decimals()
+            assert validate_and_normalise(input) == decimals.default()
 
     def test_text_should_default(self):
-        assert decimals.validate_and_normalise("10") == default_decimals()
-        assert decimals.validate_and_normalise("1") == default_decimals()
-        assert decimals.validate_and_normalise("abc") == default_decimals()
+        assert validate_and_normalise("10") == decimals.default()
+        assert validate_and_normalise("1") == decimals.default()
+        assert validate_and_normalise("abc") == decimals.default()
