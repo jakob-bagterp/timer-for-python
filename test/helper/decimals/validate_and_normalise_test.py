57a6b28737ef526062d56d58ff790f389e527403
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-04 17:13:15 +0100

    Test: Rename functions and use new minimum constant

--- test/helper/decimals/validate_and_normalise_test.py
+++ test/helper/decimals/validate_and_normalise_test.py
@@ -3,8 +3,8 @@ from timer.constants import decimals
 from timer.helper.decimals import validate_and_normalise
 
 class TestDecimalsValidationAndNormalisation():
-    def test_number_from_0_to_9(self):
-        for input in range(0, decimals.maximum() + 1):
+    def test_number_from_minimum_to_maximum(self):
+        for input in range(decimals.minimum(), decimals.maximum() + 1):
             assert validate_and_normalise(input) == input
     
     def test_number_larger_than_maximum_should_default(self):
@@ -15,8 +15,8 @@ class TestDecimalsValidationAndNormalisation():
         for input in range(-1, -100):
             assert validate_and_normalise(input) == decimals.default()
     
-    def test_float_within_maximum_should_not_default(self):
-        for input in numpy.arange(0, decimals.maximum() + 0.4, 0.1):
+    def test_float_within_minimum_to_maximum_should_not_default(self):
+        for input in numpy.arange(decimals.minimum(), decimals.maximum() + 0.4, 0.1):
             assert validate_and_normalise(input) == int(round(input))
 
     def test_float_larger_than_maximum_should_default(self):
