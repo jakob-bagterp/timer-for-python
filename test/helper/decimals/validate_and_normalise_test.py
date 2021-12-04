81b54384c6f1662af5450557ccf8d6b58b470bc3
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-04 11:50:14 +0100

    Test: Add decimals.validate_and_normalise

---
+++ test/helper/decimals/validate_and_normalise_test.py
@@ -0,0 +1,20 @@
+import timer.helper.decimals as decimals
+
+class TestDecimalsValidationAndNormalisation():
+    def test_range_0_10(self):
+        for input in range(0, 10):
+            assert decimals.validate_and_normalise(input) == input
+    
+    def test_larger_than_9_should_default(self):
+        assert decimals.validate_and_normalise(10) == 9
+
+    def test_negative_number_should_default(self):
+        assert decimals.validate_and_normalise(-1) == 2
+    
+    def test_float_should_default(self):
+        assert decimals.validate_and_normalise(3.5) == 2
+
+    def test_text_should_default(self):
+        assert decimals.validate_and_normalise("10") == 2
+        assert decimals.validate_and_normalise("1") == 2
+        assert decimals.validate_and_normalise("abc") == 2
