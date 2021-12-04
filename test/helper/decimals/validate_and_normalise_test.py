684428d9c5fada759a60693801018fee2b50f411
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-04 15:23:25 +0100

    Test: Use NumPy to test decimals validate_and_normalise (part 3)

--- test/helper/decimals/validate_and_normalise_test.py
+++ test/helper/decimals/validate_and_normalise_test.py
@@ -14,14 +14,15 @@ class TestDecimalsValidationAndNormalisation():
     def test_negative_number_should_default(self):
         for input in range(-1, -100):
             assert validate_and_normalise(input) == decimals.default()
-    
-    def test_float_should_default(self):
-        assert decimals.validate_and_normalise(3.5) == 2
-        assert decimals.validate_and_normalise(-10.1) == 2
-    
+
     def test_whole_number_as_float_should_not_default(self):
         for input in [input for input in numpy.arange(0, decimals.maximum() + 0.4, 0.1) if input % 1 == 0]:
             assert validate_and_normalise(input) == round(input, 0)
+
+    def test_float_larger_than_9_should_default(self):
+        for input in [input for input in numpy.arange(decimals.maximum() + 0.5, 100, 0.1)]:
+            print(input)
+            assert validate_and_normalise(input) == decimals.maximum()
 
     def test_negative_float_should_default(self):
         for input in numpy.arange(-100, 0, 0.1):
