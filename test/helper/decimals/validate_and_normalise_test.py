66fca74d7bf4946eaa2068b34ac40b740304198b
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-04 14:16:00 +0100

    Test: Use constants to test default decimals

--- test/helper/decimals/validate_and_normalise_test.py
+++ test/helper/decimals/validate_and_normalise_test.py
@@ -1,4 +1,5 @@
 import numpy
+from timer.constants.decimals import default as default_decimals
 import timer.helper.decimals as decimals
 
 class TestDecimalsValidationAndNormalisation():
@@ -12,7 +13,7 @@ class TestDecimalsValidationAndNormalisation():
 
     def test_negative_number_should_default(self):
         for input in range(-1, -100):
-            assert decimals.validate_and_normalise(input) == 2
+            assert decimals.validate_and_normalise(input) == default_decimals()
     
     def test_float_should_default(self):
         assert decimals.validate_and_normalise(3.5) == 2
@@ -20,9 +21,9 @@ class TestDecimalsValidationAndNormalisation():
 
     def test_negative_float_should_default(self):
         for input in numpy.arange(-100, 0, 0.1):
-            assert decimals.validate_and_normalise(input) == 2
+            assert decimals.validate_and_normalise(input) == default_decimals()
 
     def test_text_should_default(self):
-        assert decimals.validate_and_normalise("10") == 2
-        assert decimals.validate_and_normalise("1") == 2
-        assert decimals.validate_and_normalise("abc") == 2
+        assert decimals.validate_and_normalise("10") == default_decimals()
+        assert decimals.validate_and_normalise("1") == default_decimals()
+        assert decimals.validate_and_normalise("abc") == default_decimals()
