63cf213c5cf2c43ffd9420c29de1f36b5cfffd68
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-05 12:19:17 +0100

    Test: Minor rename singleton function

--- test/various/singleton_test.py
+++ test/various/singleton_test.py
@@ -1,7 +1,7 @@
 from timer import Timer
 
 class TestSingleton():
-    def test_only_one_instance(self):
+    def test_only_one_instance_of_timer(self):
         timer_1 = Timer()
         timer_2 = Timer()
         assert timer_2 is timer_1
