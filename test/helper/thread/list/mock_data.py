1246398efefd0c0f2b21af33fb09b46a7b9418c4
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-05 14:46:25 +0100

    Test: Add test of thread list remove function

--- test/helper/thread/list/mock_data.py
+++ test/helper/thread/list/mock_data.py
@@ -1,5 +1,6 @@
 import time
 import timer.constants as constants
+from timer import Timer
 from timer.model.thread_item import ThreadItem
 
 def thread_item_default() -> ThreadItem:
@@ -28,3 +29,12 @@ def thread_item_c() -> ThreadItem:
 
 def empty_threads_list() -> list[ThreadItem]:
     return []
+
+def generate_timer_with_threads_list() -> Timer:
+    timer = Timer()
+    timer.threads = [
+        thread_item_default,
+        thread_item_a,
+        thread_item_b,
+        thread_item_c]
+    return timer
