b4c710c2488b807b95cc2b1aad00e20d7f2c4cdf
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-05 15:02:27 +0100

    Test: Rename constants and variables in decimals validation so it's easier to read

--- test/helper/decimals/validate_and_normalise_test.py
+++ test/helper/decimals/validate_and_normalise_test.py
@@ -1,33 +1,33 @@
 import numpy
-from timer.constants import decimals
+from timer.constants.decimals import default, maximum, minimum 
 from timer.helper.decimals import validate_and_normalise
 
 class TestDecimalsValidationAndNormalisation():
     def test_number_from_minimum_to_maximum(self):
-        for input in range(decimals.minimum(), decimals.maximum() + 1):
-            assert validate_and_normalise(input) == input
+        for decimals in range(minimum(), maximum() + 1):
+            assert validate_and_normalise(decimals) == decimals
     
     def test_number_larger_than_maximum_should_default(self):
-        for input in range(decimals.maximum() + 1, 100):
-            assert validate_and_normalise(input) == decimals.maximum()
+        for decimals in range(maximum() + 1, 100):
+            assert validate_and_normalise(decimals) == maximum()
 
     def test_negative_number_should_default(self):
-        for input in range(-1, -100):
-            assert validate_and_normalise(input) == decimals.default()
+        for decimals in range(-1, -100):
+            assert validate_and_normalise(decimals) == default()
     
     def test_float_within_minimum_to_maximum_should_not_default(self):
-        for input in numpy.arange(decimals.minimum(), decimals.maximum() + 0.4, 0.1):
-            assert validate_and_normalise(input) == int(round(input))
+        for decimals in numpy.arange(minimum(), maximum() + 0.4, 0.1):
+            assert validate_and_normalise(decimals) == int(round(decimals))
 
     def test_float_larger_than_maximum_should_default(self):
-        for input in numpy.arange(decimals.maximum() + 0.5, 100, 0.1):
-            assert validate_and_normalise(input) == decimals.maximum()
+        for decimals in numpy.arange(maximum() + 0.5, 100, 0.1):
+            assert validate_and_normalise(decimals) == maximum()
 
     def test_negative_float_should_default(self):
-        for input in numpy.arange(-100, 0, 0.1):
-            assert validate_and_normalise(input) == decimals.default()
+        for decimals in numpy.arange(-100, 0, 0.1):
+            assert validate_and_normalise(decimals) == default()
 
     def test_text_should_default(self):
-        assert validate_and_normalise("10") == decimals.default()
-        assert validate_and_normalise("1") == decimals.default()
-        assert validate_and_normalise("abc") == decimals.default()
+        assert validate_and_normalise("10") == default()
+        assert validate_and_normalise("1") == default()
+        assert validate_and_normalise("abc") == default()
