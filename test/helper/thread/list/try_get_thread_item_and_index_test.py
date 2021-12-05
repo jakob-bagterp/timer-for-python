107e6654b0a0268254c1f929d4136f3cc48fd1df
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-05 16:29:17 +0100

    Test: Add test of thread list helper test_try_get_thread_item_and_index()

---
+++ test/helper/thread/list/try_get_thread_item_and_index_test.py
@@ -0,0 +1,24 @@
+from timer import Timer
+import timer.constants as constants
+import timer.helper.thread as thread
+from mock_data import thread_item_default, thread_item_a, thread_item_b, thread_item_c
+
+class TestTryGetThreadTtemAndIndexFromThreadList():
+    def test_try_get_thread_item_and_index(self):
+        timer = Timer()
+        assert len(timer.threads) == 0
+        _thread_item_default = thread_item_default()
+        _thread_item_a = thread_item_a()
+        _thread_item_b = thread_item_b()
+        _thread_item_c = thread_item_c()
+        timer.threads = [
+            _thread_item_default,
+            _thread_item_a,
+            _thread_item_b,
+            _thread_item_c]
+        assert len(timer.threads) == 4
+        assert _thread_item_default, 0 == thread.list.try_get_thread_item_and_index(timer, constants.none_value())
+        assert _thread_item_a, 1 == thread.list.try_get_thread_item_and_index(timer, "A")
+        assert _thread_item_b, 2 == thread.list.try_get_thread_item_and_index(timer, "B")
+        assert _thread_item_c, 3 == thread.list.try_get_thread_item_and_index(timer, "C")
+        del timer
