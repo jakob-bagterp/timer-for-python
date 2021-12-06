b11c0eaae53fb9ff7e59ad03a35e858cb19f890b
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-06 07:49:38 +0100

    Test: Add test of output message with days

--- test/helper/time_fractions/output_message_test.py
+++ test/helper/time_fractions/output_message_test.py
@@ -1,7 +1,7 @@
 from timer.constants import none_value
 from timer.helper.output import message
 from timer.helper.time_fractions import TimeFractions
-from mock_data_time_fractions import random_days_as_ns
+from mock_data_time_fractions import random_days_as_ns, random_hours_as_ns
 from mock_data_output_message import random_decimals
 
 class TestOutputMessage():
@@ -14,3 +14,13 @@ class TestOutputMessage():
             message(none_value(), fractions, decimals)
             terminal_output, _ = capfd.readouterr()
             assert terminal_output == f"Elapsed time: {time.days}d {time.hours}h {time.minutes}m {fractions.seconds_rounded()}s\n"
+
+    def test_output_message_hours(self, capfd):
+        for _ in range(100):
+            mock_elapsed_time_ns = random_hours_as_ns(allow_zero = False)
+            fractions = TimeFractions(mock_elapsed_time_ns)
+            time = fractions.time
+            decimals = random_decimals()
+            message(none_value(), fractions, decimals)
+            terminal_output, _ = capfd.readouterr()
+            assert terminal_output == f"Elapsed time: {time.hours}h {time.minutes}m {fractions.seconds_rounded()}s\n"
