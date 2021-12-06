28ae0f8652e73a09118550d3275bf99d9635b0d6
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-06 18:01:47 +0100

    Test: Make maximum in random_decimals adjustable

--- test/helper/time_fractions/mock_data_output_message.py
+++ test/helper/time_fractions/mock_data_output_message.py
@@ -1,5 +1,5 @@
 from random import randint
 from timer.constants.decimals import maximum, minimum
 
-def random_decimals() -> int:
-    return randint(minimum(), maximum())
+def random_decimals(max: int = maximum()) -> int:
+    return randint(minimum(), max if minimum() < max <= maximum() else maximum())
