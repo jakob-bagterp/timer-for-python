1deda830382129e1c3ed538a7327b428f1fc68e3
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-06 17:41:03 +0100

    Test: Refactor common process of output message into shared function

--- test/helper/time_fractions/output_message_test.py
+++ test/helper/time_fractions/output_message_test.py
@@ -4,33 +4,29 @@ from timer.helper.time_fractions import TimeFractions
 from mock_data_time_fractions import random_days_as_ns, random_hours_as_ns, random_minutes_as_ns
 from mock_data_output_message import random_decimals
 
+def process_output_message(elapsed_time_ns: int, capfd: object) -> str:
+    fractions = TimeFractions(elapsed_time_ns)
+    time = fractions.time
+    decimals = random_decimals()
+    message(none_value(), fractions, decimals)
+    terminal_output, _ = capfd.readouterr()
+    return terminal_output, fractions, time, decimals
+
 class TestOutputMessage():
     def test_output_message_days(self, capfd):
         for _ in range(100):
             mock_elapsed_time_ns = random_days_as_ns(allow_zero = False)
-            fractions = TimeFractions(mock_elapsed_time_ns)
-            time = fractions.time
-            decimals = random_decimals()
-            message(none_value(), fractions, decimals)
-            terminal_output, _ = capfd.readouterr()
+            terminal_output, fractions, time, _ = process_output_message(mock_elapsed_time_ns, capfd)
             assert terminal_output == f"Elapsed time: {time.days}d {time.hours}h {time.minutes}m {fractions.seconds_rounded()}s\n"
 
     def test_output_message_hours(self, capfd):
         for _ in range(100):
             mock_elapsed_time_ns = random_hours_as_ns(allow_zero = False)
-            fractions = TimeFractions(mock_elapsed_time_ns)
-            time = fractions.time
-            decimals = random_decimals()
-            message(none_value(), fractions, decimals)
-            terminal_output, _ = capfd.readouterr()
+            terminal_output, fractions, time, _ = process_output_message(mock_elapsed_time_ns, capfd)
             assert terminal_output == f"Elapsed time: {time.hours}h {time.minutes}m {fractions.seconds_rounded()}s\n"
 
     def test_output_message_minutes(self, capfd):
         for _ in range(100):
             mock_elapsed_time_ns = random_minutes_as_ns(allow_zero = False)
-            fractions = TimeFractions(mock_elapsed_time_ns)
-            time = fractions.time
-            decimals = random_decimals()
-            message(none_value(), fractions, decimals)
-            terminal_output, _ = capfd.readouterr()
+            terminal_output, fractions, time, decimals = process_output_message(mock_elapsed_time_ns, capfd)
             assert terminal_output == f"Elapsed time: {fractions.count_minutes_to_seconds():.{decimals}f} seconds ({time.minutes}m {fractions.seconds_rounded()}s)\n"
