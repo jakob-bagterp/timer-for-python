9d986f9e73ec6b90976764a66757371934742654
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-06 18:04:23 +0100

    Test: Make maximum in random_decimals adjustable (part 2)

--- test/helper/time_fractions/output_message_test.py
+++ test/helper/time_fractions/output_message_test.py
@@ -5,10 +5,10 @@ from timer.model.elapsed_time_fractions import ElapsedTimeFractions
 from mock_data_time_fractions import random_days_as_ns, random_hours_as_ns, random_minutes_as_ns, random_seconds_as_ns, random_milliseconds_as_ns, random_microseconds_as_ns, random_nanoseconds_as_ns
 from mock_data_output_message import random_decimals
 
-def process_output_message(elapsed_time_ns: int, capfd: object) -> tuple[str, TimeFractions, ElapsedTimeFractions, int]:
+def process_output_message(elapsed_time_ns: int, capfd: object, max_decimals: int = None) -> tuple[str, TimeFractions, ElapsedTimeFractions, int]:
     fractions = TimeFractions(elapsed_time_ns)
     time = fractions.time
-    decimals = random_decimals()
+    decimals = random_decimals() if max_decimals is None else random_decimals(max_decimals)
     message(none_value(), fractions, decimals)
     terminal_output, _ = capfd.readouterr()
     return terminal_output, fractions, time, decimals
