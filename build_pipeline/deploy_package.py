89785cb20ceb307ef1d39532111925ff109fc8ce
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-08 20:07:14 +0100

    Pipeline: Rename deploy to PyPI functions

--- build_pipeline/deploy_package.py
+++ build_pipeline/deploy_package.py
@@ -2,9 +2,9 @@ import subprocess
 from helper.config import destination
 from helper import working_directory
 
-def deploy_package() -> None:
+def deploy_to_pypi() -> None:
     working_directory.set_as_project_base_path()
     subprocess.call(f"twine upload {destination()}/*".split())
 
 if __name__ == "__main__":
-    deploy_package()
+    deploy_to_pypi()
--- build_pipeline/deploy_test_package.py
+++ build_pipeline/deploy_test_package.py
@@ -2,9 +2,9 @@ import subprocess
 from helper.config import destination
 from helper import working_directory
 
-def deploy_test_package() -> None:
+def deploy_to_test_pypi() -> None:
     working_directory.set_as_project_base_path()
     subprocess.call(f"python3 -m twine upload --repository testpypi {destination()}/*".split())
 
 if __name__ == "__main__":
-    deploy_test_package()
+    deploy_to_test_pypi()
