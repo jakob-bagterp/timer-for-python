f5cdcd8fd551ab42adaa725fc34ab3e0e02cc3f4
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-23 23:26:30 +0100

    Pipeline: Add helper functions to get version and file name of latest release

--- build_pipeline/helper/__init__.py
+++ build_pipeline/helper/__init__.py
@@ -1,1 +1,9 @@
 __all__ = ["deploy_package", "directory", "build_package", "run_tests"]
+
+def get_version() -> str:
+    from timer.version import __version__
+    return __version__
+
+def get_release_file_name() -> str:
+    from config import package_name
+    return f"{package_name()}-{get_version()}.tar.gz"
