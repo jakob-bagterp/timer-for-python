fa5e6cdbcc63e133d778efadaf02986d3ff6b8da
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-24 13:24:06 +0100

    Correct and add comments to config constants

--- build_pipeline/config/directory.py
+++ build_pipeline/config/directory.py
@@ -1,7 +1,9 @@
 def temp_builds() -> str:
-    """Destination directory "dist" viewed from the project base. Usage examples: f"./{constants.build_directory()}" as directory ("./dist") or f"{constants.build_directory()/*}" as glob ("dist/*")."""
+    """Destination directory "dist" viewed from the project base. Usage examples: f"./{config.temp_builds()}" as directory ("./dist") or f"{config.temp_builds()/*}" as glob ("dist/*")."""
 
     return "dist"
 
 def releases() -> str:
+    """Directory of different final version of release packages, e.g. to be distributed via Homebrew."""
+    
     return "releases"
