721498a1d8c2e4bdb02cdd7f811a66f74cf6c78e
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-27 00:37:59 +0200

    Refactor imports of helper functions

--- build_pipeline/helper/__init__.py
+++ build_pipeline/helper/__init__.py
@@ -1,18 +1,18 @@
 __all__ = ["deploy_package", "directory", "build_package", "run_tests"]
 
 import subprocess
+from timer.version import __version__
+from config import package_name
+from config.directory import releases
+from . import directory 
 
 def get_version() -> str:
-    from timer.version import __version__
     return __version__
 
 def get_release_file_name() -> str:
-    from config import package_name
     return f"{package_name()}-{get_version()}.tar.gz"
 
 def output_release_file_checksum() -> None:
-    from config.directory import releases
-    from . import directory 
     directory.working.set_as_project_base_path()
     release_file_name = get_release_file_name()
     print(f"Checksum SHA256 for {release_file_name}:")
