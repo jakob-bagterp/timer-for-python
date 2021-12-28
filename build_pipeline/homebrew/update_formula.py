e65f4ae0d45bfe0e311a9a39adc481dbf7755316
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-29 00:50:09 +0200

    Homebrew: Add update, upgrade, audit of Homebrew to update script (reference: https://thecoatlessprofessor.com/programming/macos/updating-a-homebrew-formula/)

--- build_pipeline/homebrew/update_formula.py
+++ build_pipeline/homebrew/update_formula.py
@@ -1,7 +1,21 @@
+import subprocess
 from shutil import copyfile
 from pathlib import Path
 from config import directory, package_install_name
 
+def homebrew_update() -> None:
+    command = "brew update"
+    print(f"Executing command \"{command}\"...")
+    subprocess.call(command.split())
+
+def homebrew_upgrade() -> None:
+    command = "brew upgrade"
+    print(f"Executing command \"{command}\"...")
+    subprocess.call(command.split())
+
+def homebrew_audit_package() -> None:
+    subprocess.call(f"brew audit --strict --online {package_install_name()}".split())
+ 
 def copy_formula_to_homebrew_formulas() -> None:
     path_of_this_file = Path(__file__)
     path_of_this_directory = path_of_this_file.parent.absolute()
@@ -10,4 +24,7 @@ def copy_formula_to_homebrew_formulas() -> None:
     copyfile(source, destination)
 
 if __name__ == "__main__":
+    homebrew_update()
+    homebrew_upgrade()
+    homebrew_audit_package()
     copy_formula_to_homebrew_formulas()
