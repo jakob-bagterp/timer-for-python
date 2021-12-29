fc8605ae441d265022726cd79fcef39ba8d2f025
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-29 13:22:38 +0200

    Homebrew: WIP for Git flow check status and stage file (reference: https://thecoatlessprofessor.com/programming/macos/updating-a-homebrew-formula/)

--- build_pipeline/helper/directory/working.py
+++ build_pipeline/helper/directory/working.py
@@ -1,7 +1,11 @@
 import os
 from pathlib import Path
+from config import directory
 
 def set_as_project_base_path() -> None:
     path_of_this_file = Path(__file__)
     project_base_directory = path_of_this_file.parent.parent.parent.parent.absolute() # The parent directory of the "build_pipeline" directory.
     os.chdir(project_base_directory)
+
+def set_as_homebrew_core() -> None:
+    os.chdir(directory.homebrew_core())
--- build_pipeline/homebrew/update_formula.py
+++ build_pipeline/homebrew/update_formula.py
@@ -1,7 +1,8 @@
 from shutil import copyfile
 from pathlib import Path
 from config import directory, package_install_name
-from helper import confirm_to_proceed, directory, execute_command_and_print
+from helper import confirm_to_proceed, execute_command_and_print
+from helper.directory import working as working_directory
 
 def homebrew_update() -> None:
     execute_command_and_print("brew update")
@@ -19,9 +20,20 @@ def copy_formula_to_homebrew_formulas() -> None:
     destination = f"{directory.homebrew_formulas()}/{package_install_name()}.rb"
     copyfile(source, destination)
 
+def homebrew_git_check_status() -> None:
+    working_directory.set_as_homebrew_core()
+    execute_command_and_print("git status")
+
+def homebrew_git_stage_file(filename: str) -> None:
+    working_directory.set_as_homebrew_core()
+    execute_command_and_print(f"git add {filename}")
+
 if __name__ == "__main__":
     homebrew_update()
     homebrew_upgrade()
     homebrew_audit_package()
-    confirm_to_proceed() # If any errors or extraordinary manual updates are needed.
+    confirm_to_proceed("Continue?") # If any errors or extraordinary manual updates are needed.
     copy_formula_to_homebrew_formulas()
+    homebrew_git_stage_file(f"Formula/{package_install_name()}.rb")
+    homebrew_git_check_status()
+    confirm_to_proceed("Commit changes and create pull request?")
