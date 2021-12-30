e1af25958b2857137c0796fe4d181f9a318132db
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-30 12:28:11 +0200

    Homebrew: Minor update of description of formula.

--- build_pipeline/homebrew/formula.rb
+++ build_pipeline/homebrew/formula.rb
@@ -1,5 +1,5 @@
 class TimerForPython < Formula
-  desc "Makes it easy to measure how much time it takes to run Python programs"
+  desc "Makes it easy to measure time and performance of Python programs"
   homepage "https://github.com/jakob-bagterp/timer_for_python"
   url "https://github.com/jakob-bagterp/timer_for_python/releases/download/v0.3.3/timer_for_python-0.3.3.tar.gz"
   sha256 "71f49d1b98fb05dbc572a34c2edf1411fbf42ab4ba0063e2bf7c9cb36cf38d98"
