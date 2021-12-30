6ef5cb3fb97999d934b51c2fec05eca887736327
Jakob Bagterp <jakob_bagterp@hotmail.com>
2021-12-30 02:12:19 +0200

    Homebrew: Fix audit error "Use 2 (not 4) spaces for indentation".

--- build_pipeline/homebrew/formula.rb
+++ build_pipeline/homebrew/formula.rb
@@ -1,17 +1,17 @@
 class TimerForPython < Formula
-    desc "Timer for Python makes it easy for beginners and experts to measure how much time it takes to run Python programs and gauge performance of multiple, smaller bits of code."
-    homepage "https://github.com/jakob-bagterp/timer_for_python"
-    url "https://github.com/jakob-bagterp/timer_for_python/releases/download/v0.3.3/timer_for_python-0.3.3.tar.gz"
-    sha256 "71f49d1b98fb05dbc572a34c2edf1411fbf42ab4ba0063e2bf7c9cb36cf38d98"
-    license "MIT"
+  desc "Timer for Python makes it easy for beginners and experts to measure how much time it takes to run Python programs and gauge performance of multiple, smaller bits of code."
+  homepage "https://github.com/jakob-bagterp/timer_for_python"
+  url "https://github.com/jakob-bagterp/timer_for_python/releases/download/v0.3.3/timer_for_python-0.3.3.tar.gz"
+  sha256 "71f49d1b98fb05dbc572a34c2edf1411fbf42ab4ba0063e2bf7c9cb36cf38d98"
+  license "MIT"
 
-    depends_on "python@3.8" => :optional
+  depends_on "python@3.8" => :optional
 
-    def install
-        bin.install "timer-for-python"
-    end
+  def install
+    bin.install "timer-for-python"
+  end
 
-    test do
-        system "false"
-    end
+  test do
+    system "false"
+  end
 end
