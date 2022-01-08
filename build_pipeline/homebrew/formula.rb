4efba53e7487867954630e790e2cb89e8e16bda7
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-08 12:40:08 +0100

    Fix release download link.

--- build_pipeline/homebrew/formula.rb
+++ build_pipeline/homebrew/formula.rb
@@ -1,7 +1,7 @@
 class TimerForPython < Formula
   desc "Makes it easy to measure time and performance of Python programs"
   homepage "https://github.com/jakob-bagterp/timer-for-python"
-  url "https://github.com/jakob-bagterp/timer-for-python/releases/download/v0.3.3/timer-for-python-0.3.3.tar.gz"
+  url "https://github.com/jakob-bagterp/timer-for-python/releases/download/v0.3.3/timer_for_python-0.3.3.tar.gz"
   sha256 "71f49d1b98fb05dbc572a34c2edf1411fbf42ab4ba0063e2bf7c9cb36cf38d98"
   license "MIT"
 
