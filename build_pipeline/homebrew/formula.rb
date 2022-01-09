b81182ad0250c79ca9f96386a16d341d2b3891e6
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-10 00:58:51 +0100

    Homebrew: Update formula following succesfull release in tap homebrew-timer-for-python homebrew-timer-for-pythonx.

--- build_pipeline/homebrew/formula.rb
+++ build_pipeline/homebrew/formula.rb
@@ -1,21 +1,19 @@
 class TimerForPython < Formula
+  include Language::Python::Virtualenv
+
   desc "Makes it easy to measure time and performance of Python programs"
   homepage "https://github.com/jakob-bagterp/timer-for-python"
   url "https://github.com/jakob-bagterp/timer-for-python/releases/download/v0.3.3/timer_for_python-0.3.3.tar.gz"
   sha256 "71f49d1b98fb05dbc572a34c2edf1411fbf42ab4ba0063e2bf7c9cb36cf38d98"
   license "MIT"
 
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+    virtualenv_install_with_resources
   end
 
   test do
-    system "false"
+    system "true"
   end
 end
