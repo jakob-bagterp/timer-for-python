bb0d7b5c39dc4bbfd4bb2ce5aa57dc2ca047850a
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-11 19:19:17 +0100

    Bump version to 0.3.4.

--- build_pipeline/homebrew/formula.rb
+++ build_pipeline/homebrew/formula.rb
@@ -3,8 +3,8 @@ class TimerForPython < Formula
 
   desc "Makes it easy to measure time and performance of Python programs"
   homepage "https://github.com/jakob-bagterp/timer-for-python"
-  url "https://github.com/jakob-bagterp/timer-for-python/releases/download/v0.3.3/timer_for_python-0.3.3.tar.gz"
-  sha256 "71f49d1b98fb05dbc572a34c2edf1411fbf42ab4ba0063e2bf7c9cb36cf38d98"
+  url "https://github.com/jakob-bagterp/timer-for-python/releases/download/v0.3.4/timer-for-python-0.3.4.tar.gz"
+  sha256 "ab64da1a76157f56595fc972016fc260d5467e5f66e3dce5b4fb20fd002da322"
   license "MIT"
 
   depends_on "python@3.9"
