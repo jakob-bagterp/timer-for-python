a1d85ab4dbf957e46e87d182abaeee81822656ea
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-17 16:55:07 +0100

    Fix type issue controller.stop() by ensuring guard clause handles tuple of return types from try_get_thread_item_and_index helper function.

--- src/timer/controller/__init__.py
+++ src/timer/controller/__init__.py
@@ -21,7 +21,7 @@ def stop(timer: TimerObject, thread: Union[str, None], stop_time: int) -> None:
     try:
         thread = helper.thread.normalise_to_string_and_uppercase(thread)
         thread_item, entry_index = helper.thread.list.try_get_thread_item_and_index(timer, thread)
-        if thread_item is not None: # If there's a match in existing threads, return values to the stop function and remove the entry.
+        if thread_item is not None and entry_index is not None: # If there's a match in existing threads, return values to the stop function and remove the entry.
             helper.thread.list.remove(timer, entry_index)
             elapsed_time = stop_time - thread_item.start_time
             time_fractions = TimeFractions(elapsed_time)
