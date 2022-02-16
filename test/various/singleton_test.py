fde304a79f1c563479132fc81ce1f8d5570fb869
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-16 23:05:07 +0100

    Add extra singleton tests.

--- test/various/singleton_test.py
+++ test/various/singleton_test.py
@@ -5,3 +5,18 @@ class TestSingleton():
         timer_1 = Timer()
         timer_2 = Timer()
         assert timer_2 is timer_1
+
+    def test_only_one_instance_of_timer_with_decimals_1(self) -> None:
+        timer_1 = Timer(decimals = 5)
+        timer_2 = Timer(decimals = 5)
+        assert timer_2 is timer_1
+
+    def test_only_one_instance_of_timer_with_decimals_2(self) -> None:
+        timer_1 = Timer(decimals = 3)
+        timer_2 = Timer(decimals = 5)
+        assert timer_2 is timer_1
+
+    def test_only_one_instance_of_timer_with_decimals_3(self) -> None:
+        timer_1 = Timer()
+        timer_2 = Timer(decimals = 5)
+        assert timer_2 is timer_1
