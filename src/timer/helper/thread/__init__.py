21b2c57d1a05b717f501bc89a1582205150b736b
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-16 23:13:59 +0100

    Update expression based on suggestion from Sourcery.

--- src/timer/helper/thread/__init__.py
+++ src/timer/helper/thread/__init__.py
@@ -4,7 +4,7 @@ from . import list
 from ...constants import none_value
 
 def is_none(thread: str | None) -> bool:
-    return True if thread is None or thread == none_value() else False
+    return thread is None or thread == none_value()
 
 def normalise_to_string_and_uppercase(thread: str | None) -> str: # The thread list iterator only supports strings and numbers and not None, hence the renaming to "NONE".
     return none_value() if is_none(thread) else str(thread).upper()
