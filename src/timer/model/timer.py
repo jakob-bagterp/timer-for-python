6e10cc2e5f27cf9cc68dde3d0b5216a07f903478
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-15 22:46:20 +0100

    Add __enter__ and __exit__ methods for context manager, and update __new__ and __init__ methods to accommodate required context arguments.

--- src/timer/model/timer.py
+++ src/timer/model/timer.py
@@ -9,15 +9,28 @@ class Timer(TimerBase):
 
     _instance = None
 
-    def __new__(cls, decimals: int = constant.decimals.DEFAULT):
+    def __new__(cls, thread: str | None = None, decimals: int = constant.decimals.DEFAULT):
         if not cls._instance:  # Singleton: Ensure there's only a single instance of Timer running.
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def __init__(self, decimals: int = constant.decimals.DEFAULT) -> None:
-        self.threads: list[ThreadItem] = []
+    def __init__(self, thread: str | None = None, decimals: int = constant.decimals.DEFAULT) -> None:
         self.decimals: int = decimals if decimals == constant.decimals.DEFAULT else helper.decimals.validate_and_normalise(
             decimals)
+        if thread is None:
+            self.threads: list[ThreadItem] = []
+        else:
+            self.threads: list[ThreadItem] = [ThreadItem(
+                name=helper.thread.normalise_to_string_and_uppercase(thread),
+                start_time=time.perf_counter_ns(),
+                decimals=self.decimals
+            )]
+
+    def __enter__(self, thread: str | None = None, decimals: int | None = None):
+        return self.start(thread, decimals)
+
+    def __exit__(self, exc_type, exc_value, traceback, thread: str | None = None):
+        self.stop(thread)
 
     def start(self, thread: str | None = None, decimals: int | None = None) -> None:
         try:
