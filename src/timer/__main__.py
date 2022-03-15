c43b9fec97e26d2aca0803ab061a6c1f1b0a26d0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-15 22:17:15 +0100

    Refactor and move implementation details of abstract timer class to actual timer class.

--- src/timer/__main__.py
+++ src/timer/__main__.py
@@ -2,9 +2,23 @@ import time
 
 from . import controller, error
 from .model.timer import TimerObject
-
+from . import constant, helper
+from .model.thread_item import ThreadItem
 
 class Timer(TimerObject):
+
+    _instance = None
+
+    def __new__(cls, decimals: int = constant.decimals.DEFAULT):
+        if not cls._instance:  # Singleton: Ensure there's only a single instance of Timer running.
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, decimals: int = constant.decimals.DEFAULT) -> None:
+        self.threads: list[ThreadItem] = []
+        self.decimals: int = decimals if decimals == constant.decimals.DEFAULT else helper.decimals.validate_and_normalise(
+            decimals)
+
     def start(self, thread: str | None = None, decimals: int | None = None) -> None:
         try:
             start_time = time.perf_counter_ns()  # For precision, this is the first operation of the function.
--- src/timer/model/timer.py
+++ src/timer/model/timer.py
@@ -1,23 +1,14 @@
 from abc import ABC, abstractmethod
 
-from .. import constant, helper
 from .thread_item import ThreadItem
 
 
 class TimerObject(ABC):
     """Abstract base class of Timer object."""
 
-    _instance = None
-
-    def __new__(cls, decimals: int = constant.decimals.DEFAULT):
-        if not cls._instance:  # Singleton: Ensure there's only a single instance of Timer running.
-            cls._instance = super().__new__(cls)
-        return cls._instance
-
-    def __init__(self, decimals: int = constant.decimals.DEFAULT) -> None:
-        self.threads: list[ThreadItem] = []
-        self.decimals: int = decimals if decimals == constant.decimals.DEFAULT else helper.decimals.validate_and_normalise(
-            decimals)
+    def __init__(self) -> None:
+        self.threads: list[ThreadItem]
+        self.decimals: int
 
     @abstractmethod
     def start(self, thread: str, decimals: int) -> None:
