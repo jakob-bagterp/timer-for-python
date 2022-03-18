b75c50955475258cf52cbf02e601beafa87242a0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-18 16:39:55 +0100

    Add test of decimals argument for context manager.

--- test/various/with_statement_context_manager_test.py
+++ test/various/with_statement_context_manager_test.py
@@ -1,6 +1,8 @@
+import re
 import time
 
 from timer import Timer
+from timer.constant.decimals import MAXIMUM, MINIMUM
 
 SHORT_INTERVAL: float = 0.1  # Seconds.
 
@@ -21,3 +23,19 @@ def test_with_statement_context_manager_with_thread(capfd: object) -> None:
     terminal_output, _ = capfd.readouterr()
     assert EXPECTED_TERMINAL_OUTPUT_PREFIX in terminal_output
     assert _thread in terminal_output
+
+
+DECIMALS_PATTERN = re.compile(r"(?<=\.)\d*")
+
+
+def test_with_statement_context_manager_with_decimals(capfd: object) -> None:
+    for decimals in range(MINIMUM, MAXIMUM + 1):
+        with Timer(decimals=decimals):
+            time.sleep(SHORT_INTERVAL)
+        terminal_output, _ = capfd.readouterr()
+        assert EXPECTED_TERMINAL_OUTPUT_PREFIX in terminal_output
+        decimals_output = DECIMALS_PATTERN.search(terminal_output)
+        if decimals == 0:
+            assert decimals_output is None
+        else:
+            assert decimals == len(decimals_output.group(0))
