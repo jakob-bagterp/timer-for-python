2dec02ef939857dbceeab08374c5a2082f1fad38
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-19 11:44:39 +0100

    Refactor thread names so they're randomised.

--- test/various/with_statement_context_manager_test.py
+++ test/various/with_statement_context_manager_test.py
@@ -1,6 +1,7 @@
 import time
 
 from _constant.interval import ULTRA_SHORT_INTERVAL
+from _helper.random import random_thread_name
 from _helper.terminal_output import (verify_decimals_in_terminal_output,
                                      verify_prefix_in_terminal_output)
 from _mock_data.decimals import DECIMALS_RANGE
@@ -17,7 +18,7 @@ def test_with_statement_context_manager(capfd: object) -> None:
 
 
 def test_with_statement_context_manager_with_thread(capfd: object) -> None:
-    _thread = "thread"  # TODO: Use random thread name instead.
+    _thread = random_thread_name(4)
     with Timer(thread=_thread):
         time.sleep(ULTRA_SHORT_INTERVAL)
     terminal_output, _ = capfd.readouterr()
@@ -34,8 +35,8 @@ def test_with_statement_context_manager_with_decimals(capfd: object) -> None:
         assert verify_decimals_in_terminal_output(decimals, terminal_output)
 
 
-def test_with_statement_context_manager_with_thread_and_decimals(capfd: object) -> None:
-    _thread = "thread"  # TODO: Use random thread name instead.
+def test_with_statement_context_manager_with_thread_1nd_decimals(capfd: object) -> None:
+    _thread = random_thread_name(4)
     for decimals in DECIMALS_RANGE:
         with Timer(thread=_thread, decimals=decimals):
             time.sleep(ULTRA_SHORT_INTERVAL)
@@ -46,28 +47,28 @@ def test_with_statement_context_manager_with_thread_and_decimals(capfd: object) 
 
 
 def test_with_statement_context_manager_with_multiple_nested_threads_and_decimals(capfd: object) -> None:
-    _thread_a = "A-LIST"  # TODO: Use random thread name instead.
-    _thread_b = "B-LIST"  # TODO: Use random thread name instead.
+    _thread_1 = random_thread_name(4)
+    _thread_2 = random_thread_name(5)
     for decimals in DECIMALS_RANGE:
         with Timer():
-            with Timer(thread=_thread_a, decimals=decimals):
+            with Timer(thread=_thread_1, decimals=decimals):
                 time.sleep(ULTRA_SHORT_INTERVAL)
-                with Timer(thread=_thread_b):
+                with Timer(thread=_thread_2):
                     time.sleep(ULTRA_SHORT_INTERVAL)
                 terminal_output_1, _ = capfd.readouterr()
                 assert verify_prefix_in_terminal_output(terminal_output_1)
-                assert _thread_b in terminal_output_1
+                assert _thread_2 in terminal_output_1
                 assert verify_decimals_in_terminal_output(DEFAULT, terminal_output_1)
             terminal_output_2, _ = capfd.readouterr()
             assert verify_prefix_in_terminal_output(terminal_output_2)
-            assert _thread_a in terminal_output_2
+            assert _thread_1 in terminal_output_2
             assert verify_decimals_in_terminal_output(decimals, terminal_output_2)
         terminal_output_3, _ = capfd.readouterr()
         assert verify_prefix_in_terminal_output(terminal_output_3)
 
 
 def test_mix_of_context_manager_standard(capfd: object) -> None:
-    _thread = "thread"  # TODO: Use random thread name instead + None type.
+    _thread = random_thread_name(4)
     timer = Timer()
     timer.start(thread=_thread)
     with Timer():
