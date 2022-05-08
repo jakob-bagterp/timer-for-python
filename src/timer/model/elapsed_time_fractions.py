42cbbf72dda7435dd5f5c7c40f87a56579390cbf
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-05-08 10:26:12 +0200

    Add slots to various data classes.

--- src/timer/model/elapsed_time_fractions.py
+++ src/timer/model/elapsed_time_fractions.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, slots=True)
 class ElapsedTimeFractions:
     """Class to define fractions of elapsed time."""
 
