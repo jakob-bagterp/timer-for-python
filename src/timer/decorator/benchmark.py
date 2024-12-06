36ed5154cf201dd4125b7da0b06d53b8c8d95f87
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-06 23:50:42 +0100

    First attempt to expand benchmark timer with optional arguments.

--- src/timer/decorator/benchmark.py
+++ src/timer/decorator/benchmark.py
@@ -5,10 +5,12 @@ from typing import Any
 from ..model.timer import Timer
 
 
-def benchmark_timer(function: Callable[..., Any]) -> Callable[..., Any]:
-    @functools.wraps(function)
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
-        with Timer(thread=function.__name__):
-            return function(*args, **kwargs)
-
-    return wrapper
+def benchmark_timer(thread: str | None = None, decimals: int = 2) -> Callable[..., Any]:
+    def decorator(function: Callable[..., Any]) -> Callable[..., Any]:
+        @functools.wraps(function)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            thread_name = thread if thread else function.__name__
+            with Timer(thread=thread_name, decimals=decimals):
+                return function(*args, **kwargs)
+        return wrapper
+    return decorator
