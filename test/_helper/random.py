3ad6b745afad54750d7b490631fcca5f85034e5c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-07 09:42:02 +0100

    Added docstrings to helper methods.

--- test/_helper/random.py
+++ test/_helper/random.py
@@ -5,8 +5,12 @@ from timer.constant.decimals import MAXIMUM, MINIMUM
 
 
 def random_decimals(max: int = MAXIMUM) -> int:
+    """Get a random number of decimals between the minimum 0 and maximum 9."""
+
     return randint(MINIMUM, max if MINIMUM < max <= MAXIMUM else MAXIMUM)
 
 
 def random_thread_name(length: int = randint(1, 10)) -> str:
+    """Get a random thread name with a given or random length."""
+
     return "".join(choices(ascii_uppercase, k=length))
