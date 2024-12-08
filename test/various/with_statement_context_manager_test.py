309f73941c45075c99ced189e78c773480365f92
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-08 14:21:16 +0100

    Minor renamed test variables.

--- test/various/with_statement_context_manager_test.py
+++ test/various/with_statement_context_manager_test.py
@@ -18,12 +18,12 @@ def test_with_statement_context_manager(capfd: object) -> None:
 
 
 def test_with_statement_context_manager_with_thread(capfd: object) -> None:
-    _thread = random_thread_name(4)
-    with Timer(thread=_thread):
+    thread = random_thread_name(4)
+    with Timer(thread=thread):
         time.sleep(ULTRA_SHORT_INTERVAL)
     terminal_output, _ = capfd.readouterr()
     assert verify_prefix_in_terminal_output(terminal_output)
-    assert _thread in terminal_output
+    assert thread in terminal_output
 
 
 def test_with_statement_context_manager_with_decimals(capfd: object) -> None:
@@ -36,46 +36,46 @@ def test_with_statement_context_manager_with_decimals(capfd: object) -> None:
 
 
 def test_with_statement_context_manager_with_thread_1nd_decimals(capfd: object) -> None:
-    _thread = random_thread_name(4)
+    thread = random_thread_name(4)
     for decimals in DECIMALS_RANGE:
-        with Timer(thread=_thread, decimals=decimals):
+        with Timer(thread=thread, decimals=decimals):
             time.sleep(ULTRA_SHORT_INTERVAL)
         terminal_output, _ = capfd.readouterr()
         assert verify_prefix_in_terminal_output(terminal_output)
-        assert _thread in terminal_output
+        assert thread in terminal_output
         assert verify_decimals_in_terminal_output(decimals, terminal_output)
 
 
 def test_with_statement_context_manager_with_multiple_nested_threads_and_decimals(capfd: object) -> None:
-    _thread_1 = random_thread_name(4)
-    _thread_2 = random_thread_name(5)
+    thread_1 = random_thread_name(4)
+    thread_2 = random_thread_name(5)
     for decimals in DECIMALS_RANGE:
         with Timer():
-            with Timer(thread=_thread_1, decimals=decimals):
+            with Timer(thread=thread_1, decimals=decimals):
                 time.sleep(ULTRA_SHORT_INTERVAL)
-                with Timer(thread=_thread_2):
+                with Timer(thread=thread_2):
                     time.sleep(ULTRA_SHORT_INTERVAL)
                 terminal_output_1, _ = capfd.readouterr()
                 assert verify_prefix_in_terminal_output(terminal_output_1)
-                assert _thread_2 in terminal_output_1
+                assert thread_2 in terminal_output_1
                 assert verify_decimals_in_terminal_output(DEFAULT, terminal_output_1)
             terminal_output_2, _ = capfd.readouterr()
             assert verify_prefix_in_terminal_output(terminal_output_2)
-            assert _thread_1 in terminal_output_2
+            assert thread_1 in terminal_output_2
             assert verify_decimals_in_terminal_output(decimals, terminal_output_2)
         terminal_output_3, _ = capfd.readouterr()
         assert verify_prefix_in_terminal_output(terminal_output_3)
 
 
 def test_mix_of_context_manager_standard(capfd: object) -> None:
-    _thread = random_thread_name(4)
+    thread = random_thread_name(4)
     timer = Timer()
-    timer.start(thread=_thread)
+    timer.start(thread=thread)
     with Timer():
         time.sleep(ULTRA_SHORT_INTERVAL)
     terminal_output_1, _ = capfd.readouterr()
     assert verify_prefix_in_terminal_output(terminal_output_1)
-    timer.stop(_thread)
+    timer.stop(thread)
     terminal_output_2, _ = capfd.readouterr()
-    assert _thread in terminal_output_2
+    assert thread in terminal_output_2
     assert verify_prefix_in_terminal_output(terminal_output_2)
