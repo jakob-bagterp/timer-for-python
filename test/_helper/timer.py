f822fe5e756c32b8cd90910b81d555679e1b2fc1
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-08 14:33:57 +0100

    Fixed issue in ensure_all_timer_threads_are_stopped() after update of private variables for Timer instances.

--- test/_helper/timer.py
+++ test/_helper/timer.py
@@ -4,5 +4,5 @@ from timer import Timer
 def ensure_all_timer_threads_are_stopped() -> None:
     """Ensures all threads are stopped."""
 
-    Timer().threads = []
-    assert len(Timer().threads) == 0
+    Timer()._threads = []
+    assert len(Timer()._threads) == 0
