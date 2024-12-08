3514f8a82d985e6b8992b4c309845d8dd31e9383
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-08 18:11:21 +0100

    Added start invalid thread test with context manager.

--- test/exception_handling/soft_error/start_test.py
+++ test/exception_handling/soft_error/start_test.py
@@ -1,5 +1,9 @@
+from os import linesep
+
 from _helper.timer import ensure_all_timer_threads_are_stopped
 from colorist import Color
+
+from timer import Timer
 
 
 def test_timer_start_invalid_thread_type_soft_error_1(capfd: object) -> None:
@@ -10,3 +14,18 @@ def test_timer_start_invalid_thread_type_soft_error_1(capfd: object) -> None:
     timer.start(thread=custom_thread)
     terminal_output, _ = capfd.readouterr()
     assert terminal_output == f"{Color.YELLOW}Timer: Something went wrong in the Timer's lookup module for thread {custom_thread.upper()}.{Color.OFF}\n"
+
+
+def test_timer_start_invalid_thread_type_soft_error_2(capfd: object) -> None:
+    custom_thread = "custom"
+
+    timer = ensure_all_timer_threads_are_stopped()
+    timer._threads = [custom_thread]  # Should be a list[ThreadItem] type.
+    with Timer(thread=custom_thread):
+        pass
+    terminal_output, _ = capfd.readouterr()
+    assert terminal_output == \
+        f"{Color.YELLOW}Timer: Something went wrong in the Timer's lookup module for thread {custom_thread.upper()}.{Color.OFF}{linesep}" +\
+        f"{Color.YELLOW}Timer: Something went wrong in the Timer's lookup module for thread {custom_thread.upper()}.{Color.OFF}{linesep}" +\
+        f"{Color.YELLOW}Timer for thread {custom_thread.upper()} is not running. Use .start(thread='{custom_thread.upper()}') to start it.{Color.OFF}{linesep}" +\
+        f"{Color.YELLOW}Timer: Something went wrong in the Timer's stop thread controller for thread {custom_thread.upper()}.{Color.OFF}{linesep}"
