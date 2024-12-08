47a01275ecbc68d040fd0f6762c00bab97744844
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-08 18:22:26 +0100

    Updated comments.

--- test/exception_handling/soft_error/start_test.py
+++ test/exception_handling/soft_error/start_test.py
@@ -10,7 +10,7 @@ def test_timer_start_invalid_thread_type_soft_error_1(capfd: object) -> None:
     custom_thread = "custom"
 
     timer = ensure_all_timer_threads_are_stopped()
-    timer._threads = [custom_thread]  # Should be a list[ThreadItem] type.
+    timer._threads = [custom_thread]  # Triggers issue by invalid type as it should be a list[ThreadItem] type.
     timer.start(thread=custom_thread)
     terminal_output, _ = capfd.readouterr()
     assert terminal_output == f"{Color.YELLOW}Timer: Something went wrong in the Timer's lookup module for thread {custom_thread.upper()}.{Color.OFF}\n"
@@ -20,7 +20,7 @@ def test_timer_start_invalid_thread_type_soft_error_2(capfd: object) -> None:
     custom_thread = "custom"
 
     timer = ensure_all_timer_threads_are_stopped()
-    timer._threads = [custom_thread]  # Should be a list[ThreadItem] type.
+    timer._threads = [custom_thread]  # Triggers issue by invalid type as it should be a list[ThreadItem] type.
     with Timer(thread=custom_thread):
         pass
     terminal_output, _ = capfd.readouterr()
