911f6013a0b0ca2b1f18b8989b7a3d0407a16fe2
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-08 18:48:28 +0100

    Refactored and split error tests into invalid type and normal scenarios.

--- test/exception_handling/soft_error/stop_test.py
+++ test/exception_handling/soft_error/stop_test.py
@@ -6,8 +6,6 @@ from _helper import operating_system
 from _helper.timer import ensure_all_timer_threads_are_stopped
 from colorist import Color
 
-from timer import Timer
-
 
 def test_timer_stop_unknown_thread_soft_error_1(capfd: object) -> None:
     if operating_system.is_windows():
@@ -60,41 +58,3 @@ def test_timer_stop_not_started_thread_soft_error_2(capfd: object) -> None:
     timer.stop(thread=custom_thread)
     terminal_output, _ = capfd.readouterr()
     assert terminal_output == f"{Color.YELLOW}Timer for thread {custom_thread.upper()} is not running. Use .start(thread='{custom_thread.upper()}') to start it.{Color.OFF}\n"
-
-
-def test_timer_stop_invalid_thread_type_soft_error_1(capfd: object) -> None:
-    if operating_system.is_windows():
-        pytest.skip("Skipping test for Windows due to line separator issue.")  # pragma: no cover
-        # TODO: Fix line separator issue on Windows.
-        return  # pragma: no cover
-
-    custom_thread = "custom"
-
-    timer = ensure_all_timer_threads_are_stopped()
-    timer._threads = [custom_thread]  # Triggers issue by invalid type as it should be a list[ThreadItem] type.
-    timer.stop(thread=custom_thread)
-    terminal_output, _ = capfd.readouterr()
-    assert terminal_output == \
-        f"{Color.YELLOW}Timer: Something went wrong in the Timer's lookup module for thread {custom_thread.upper()}.{Color.OFF}{linesep}" +\
-        f"{Color.YELLOW}Timer for thread {custom_thread.upper()} is not running. Use .start(thread='{custom_thread.upper()}') to start it.{Color.OFF}{linesep}" +\
-        f"{Color.YELLOW}Timer: Something went wrong in the Timer's stop thread controller for thread {custom_thread.upper()}.{Color.OFF}{linesep}"
-
-
-def test_timer_stop_invalid_thread_type_soft_error_2(capfd: object) -> None:
-    if operating_system.is_windows():
-        pytest.skip("Skipping test for Windows due to line separator issue.")  # pragma: no cover
-        # TODO: Fix line separator issue on Windows.
-        return  # pragma: no cover
-
-    custom_thread = "custom"
-
-    timer = ensure_all_timer_threads_are_stopped()
-    timer._threads = [custom_thread]  # Triggers issue by invalid type as it should be a list[ThreadItem] type.
-    with Timer(thread=custom_thread):
-        pass
-    terminal_output, _ = capfd.readouterr()
-    assert terminal_output == \
-        f"{Color.YELLOW}Timer: Something went wrong in the Timer's lookup module for thread {custom_thread.upper()}.{Color.OFF}{linesep}" +\
-        f"{Color.YELLOW}Timer: Something went wrong in the Timer's lookup module for thread {custom_thread.upper()}.{Color.OFF}{linesep}" +\
-        f"{Color.YELLOW}Timer for thread {custom_thread.upper()} is not running. Use .start(thread='{custom_thread.upper()}') to start it.{Color.OFF}{linesep}" +\
-        f"{Color.YELLOW}Timer: Something went wrong in the Timer's stop thread controller for thread {custom_thread.upper()}.{Color.OFF}{linesep}"
