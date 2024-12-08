01da080660b3fde4ca5c30b3452685006cc07a5a
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-08 14:03:06 +0100

    Added context manager to TimerBase.

--- src/timer/model/timer_base.py
+++ src/timer/model/timer_base.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from types import TracebackType
 
 from .thread_item import ThreadItem
 
@@ -11,6 +12,12 @@ class TimerBase(ABC):
     def __init__(self) -> None:
         self.decimals: int
         self.threads: list[ThreadItem]
+
+    def __enter__(self) -> "TimerBase":
+        raise NotImplementedError
+
+    def __exit__(self, exc_type: type[BaseException] | None, exc_value: BaseException | None, traceback: TracebackType | None) -> None:
+        raise NotImplementedError
 
     @abstractmethod
     def start(self, thread: str | None = None, decimals: int | None = None) -> None:
