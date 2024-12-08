7292aa1e6b8480f031637f056daf2ac6d0129073
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-08 17:49:10 +0100

    Updated variable names for consistency.

--- test/exception_handling/soft_error/stop_test.py
+++ test/exception_handling/soft_error/stop_test.py
@@ -13,14 +13,14 @@ def test_timer_stop_unknown_thread_soft_error_1(capfd: object) -> None:
         # TODO: Fix line separator issue on Windows.
         return  # pragma: no cover
 
-    thread = "custom"
+    custom_thread = "custom"
 
     timer = ensure_all_timer_threads_are_stopped()
     timer.start()
     time.sleep(0.1)
-    timer.stop(thread=thread)
+    timer.stop(thread=custom_thread)
     terminal_output, _ = capfd.readouterr()
-    assert terminal_output == rf"{Color.YELLOW}Timer for thread {thread.upper()} is not running. Use .start(thread='{thread.upper()}') to start it.{Color.OFF}{linesep}Or maybe you aren't stopping the right thread? Currently open threads: NONE{linesep}"
+    assert terminal_output == rf"{Color.YELLOW}Timer for thread {custom_thread.upper()} is not running. Use .start(thread='{custom_thread.upper()}') to start it.{Color.OFF}{linesep}Or maybe you aren't stopping the right thread? Currently open threads: NONE{linesep}"
 
 
 def test_timer_stop_unknown_thread_soft_error_2(capfd: object) -> None:
@@ -29,15 +29,15 @@ def test_timer_stop_unknown_thread_soft_error_2(capfd: object) -> None:
         # TODO: Fix line separator issue on Windows.
         return  # pragma: no cover
 
-    thread_1 = "custom_1"
-    thread_2 = "custom_2"
+    custom_thread_1 = "custom_1"
+    custom_thread_2 = "custom_2"
 
     timer = ensure_all_timer_threads_are_stopped()
-    timer.start(thread=thread_1)
+    timer.start(thread=custom_thread_1)
     time.sleep(0.1)
-    timer.stop(thread=thread_2)
+    timer.stop(thread=custom_thread_2)
     terminal_output, _ = capfd.readouterr()
-    assert terminal_output == rf"{Color.YELLOW}Timer for thread {thread_2.upper()} is not running. Use .start(thread='{thread_2.upper()}') to start it.{Color.OFF}{linesep}Or maybe you aren't stopping the right thread? Currently open threads: {thread_1.upper()}{linesep}"
+    assert terminal_output == rf"{Color.YELLOW}Timer for thread {custom_thread_2.upper()} is not running. Use .start(thread='{custom_thread_2.upper()}') to start it.{Color.OFF}{linesep}Or maybe you aren't stopping the right thread? Currently open threads: {custom_thread_1.upper()}{linesep}"
 
 
 def test_timer_stop_not_started_thread_soft_error_1(capfd: object) -> None:
@@ -48,9 +48,9 @@ def test_timer_stop_not_started_thread_soft_error_1(capfd: object) -> None:
 
 
 def test_timer_stop_not_started_thread_soft_error_2(capfd: object) -> None:
-    thread = "custom"
+    custom_thread = "custom"
 
     timer = ensure_all_timer_threads_are_stopped()
-    timer.stop(thread=thread)
+    timer.stop(thread=custom_thread)
     terminal_output, _ = capfd.readouterr()
-    assert terminal_output == f"{Color.YELLOW}Timer for thread {thread.upper()} is not running. Use .start(thread='{thread.upper()}') to start it.{Color.OFF}\n"
+    assert terminal_output == f"{Color.YELLOW}Timer for thread {custom_thread.upper()} is not running. Use .start(thread='{custom_thread.upper()}') to start it.{Color.OFF}\n"
