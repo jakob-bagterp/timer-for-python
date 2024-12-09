ab9c89968525cc91f543f7efcfd6e7fe45ca53ff
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-09 01:31:10 +0100

    Fixed issue with thread collision before decimals output tests.

--- test/timer/decimals_output_test.py
+++ test/timer/decimals_output_test.py
@@ -2,12 +2,14 @@ import re
 import time
 
 from _helper.terminal_output import get_terminal_output_regex
+from _helper.timer import ensure_all_timer_threads_are_stopped
 
 from timer import Timer
 from timer.constant.decimals import MAXIMUM, MINIMUM
 
 
 def test_timer_decimals_output(capfd: object) -> None:
+    ensure_all_timer_threads_are_stopped()
     for decimals in range(MINIMUM, MAXIMUM + 1):
         with Timer(decimals=decimals):
             time.sleep(0.001)
