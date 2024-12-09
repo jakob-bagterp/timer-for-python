20485e849ee608d41610175779c9cfca4ced5383
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-10 00:27:52 +0100

    Added double start test.

--- test/timer/exception_handling/soft_error/start_test.py
+++ test/timer/exception_handling/soft_error/start_test.py
@@ -33,3 +33,11 @@ def test_timer_start_thread_name_collision_soft_error_with_context_manager(capfd
     assert str(terminal_output).startswith(
         f"{Color.YELLOW}Timer for thread {custom_thread.upper()} is running. Use .stop(thread='{custom_thread.upper()}') to stop it.{Color.OFF}{linesep}" + "Elapsed time:")
     assert str(terminal_output).endswith(f" for thread {Color.GREEN}{custom_thread.upper()}{Color.OFF}\n")
+
+
+def test_timer_start_default_thread_twice_soft_error_without_context_manager(capfd: object) -> None:
+    timer = ensure_all_timer_threads_are_stopped()
+    timer.start()
+    timer.start()
+    terminal_output, _ = capfd.readouterr()
+    assert terminal_output == f"{Color.YELLOW}Timer is running. Use .stop() to stop it.{Color.OFF}\n"
