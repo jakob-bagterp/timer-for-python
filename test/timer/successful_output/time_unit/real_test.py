5c2054d859429e6081b38890a3d865970127cdae
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-10 16:55:03 +0100

    Refactored and simplified test_timer_time_unit_output().

--- test/timer/successful_output/time_unit/real_test.py
+++ test/timer/successful_output/time_unit/real_test.py
@@ -16,18 +16,25 @@ from timer import Timer
 @dataclass(frozen=True, slots=True)
 class TimeUnitTestSet:
     wait_seconds: float
+    thread: str | None
     expected_time_unit: TimeUnit
 
 
-SECONDS_TEST_SET = TimeUnitTestSet(wait_seconds=1, expected_time_unit=TimeUnit.SECONDS)
-MILLISECONDS_TEST_SET = TimeUnitTestSet(wait_seconds=0.001, expected_time_unit=TimeUnit.MILLISECONDS)
-MICROSECONDS_TEST_SET = TimeUnitTestSet(wait_seconds=0.000_001, expected_time_unit=TimeUnit.MICROSECONDS)
+SECONDS_TEST_SET_WITH_THREAD = TimeUnitTestSet(wait_seconds=1, expected_time_unit=TimeUnit.SECONDS, thread="custom")
+SECONDS_TEST_SET_WITHOUT_THREAD = TimeUnitTestSet(wait_seconds=1, expected_time_unit=TimeUnit.SECONDS, thread=None)
+MILLISECONDS_TEST_SET_WITH_THREAD = TimeUnitTestSet(wait_seconds=0.001, expected_time_unit=TimeUnit.MILLISECONDS, thread="custom")
+MILLISECONDS_TEST_SET_WITHOUT_THREAD = TimeUnitTestSet(wait_seconds=0.001, expected_time_unit=TimeUnit.MILLISECONDS, thread=None)
+MICROSECONDS_TEST_SET_WITH_THREAD = TimeUnitTestSet(wait_seconds=0.000_001, expected_time_unit=TimeUnit.MICROSECONDS, thread="custom")
+MICROSECONDS_TEST_SET_WITHOUT_THREAD = TimeUnitTestSet(wait_seconds=0.000_001, expected_time_unit=TimeUnit.MICROSECONDS, thread=None)
 
 
 @pytest.mark.parametrize("test_set", [
-    SECONDS_TEST_SET,
-    MILLISECONDS_TEST_SET,
-    MICROSECONDS_TEST_SET,
+    SECONDS_TEST_SET_WITH_THREAD,
+    SECONDS_TEST_SET_WITHOUT_THREAD,
+    MILLISECONDS_TEST_SET_WITH_THREAD,
+    MILLISECONDS_TEST_SET_WITHOUT_THREAD,
+    MICROSECONDS_TEST_SET_WITH_THREAD,
+    MICROSECONDS_TEST_SET_WITHOUT_THREAD,
 ])
 def test_timer_time_unit_output(test_set: TimeUnitTestSet, capfd: object) -> None:
     if operating_system.is_windows() and python_version.is_3_10():
@@ -40,23 +47,4 @@ def test_timer_time_unit_output(test_set: TimeUnitTestSet, capfd: object) -> Non
 
     terminal_output, _ = capfd.readouterr()
     expected_output_regex = successful_output_regex(time_unit=test_set.expected_time_unit)
-    assert re.fullmatch(expected_output_regex, terminal_output)
-
-
-@pytest.mark.parametrize("test_set, thread", [
-    (SECONDS_TEST_SET, "custom"),
-    (MILLISECONDS_TEST_SET, "custom"),
-    (MICROSECONDS_TEST_SET, "custom"),
-])
-def test_timer_time_unit_output_with_thread(test_set: TimeUnitTestSet, thread: str, capfd: object) -> None:
-    if operating_system.is_windows() and python_version.is_3_10():
-        pytest.skip("Skipping test for Python 3.10 on Windows since the sleep timer is flaky and inaccurate.")  # pragma: no cover
-        return  # pragma: no cover
-
-    ensure_all_timer_threads_are_stopped()
-    with Timer(thread=thread):
-        time.sleep(test_set.wait_seconds)
-
-    terminal_output, _ = capfd.readouterr()
-    expected_output_regex = successful_output_regex(thread=thread, time_unit=test_set.expected_time_unit)
     assert re.fullmatch(expected_output_regex, terminal_output)
