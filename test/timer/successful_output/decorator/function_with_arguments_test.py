f82c435e955da08f9367559db3b32b6e92e00626
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-10 21:14:14 +0100

    Added extra tests with custom thread and decimals.

--- test/timer/successful_output/decorator/function_with_arguments_test.py
+++ test/timer/successful_output/decorator/function_with_arguments_test.py
@@ -8,7 +8,11 @@ from _constant.interval import ULTRA_SHORT_INTERVAL
 from _helper.terminal_output import successful_output_regex
 from _helper.timer import ensure_all_timer_threads_are_stopped
 
+from timer.constant.decimals import DEFAULT as DEFAULT_DECIMALS
 from timer.decorator.function import function_timer
+
+CUSTOM_THREAD = "custom"
+CUSTOM_DECIMALS = 5
 
 
 @function_timer()
@@ -26,14 +30,26 @@ def function_with_args_and_kwargs(a: int, b: int, x: int = 3, y: int = 4) -> Non
     time.sleep(ULTRA_SHORT_INTERVAL)
 
 
-@pytest.mark.parametrize("function, args, kwargs, expected_thread_name", [
-    (function_with_args, (1, 2), {}, "FUNCTION_WITH_ARGS(A=1, B=2)"),
-    (function_with_kwargs, (), {"x": 3, "y": 4}, "FUNCTION_WITH_KWARGS(X=3, Y=4)"),
-    (function_with_args_and_kwargs, (1, 2), {"x": 3, "y": 4}, "FUNCTION_WITH_ARGS_AND_KWARGS(A=1, B=2, X=3, Y=4)"),
+@function_timer(thread=CUSTOM_THREAD)
+def function_with_args_kwargs_and_custom_thread(a: int, b: int, x: int = 3, y: int = 4) -> None:
+    time.sleep(ULTRA_SHORT_INTERVAL)
+
+
+@function_timer(thread=CUSTOM_THREAD, decimals=CUSTOM_DECIMALS)
+def function_with_args_kwargs_custom_thread_and_custom_decimals(a: int, b: int, x: int = 3, y: int = 4) -> None:
+    time.sleep(ULTRA_SHORT_INTERVAL)
+
+
+@pytest.mark.parametrize("function, args, kwargs, expected_thread_name, decimals", [
+    (function_with_args, (1, 2), {}, "FUNCTION_WITH_ARGS(A=1, B=2)", DEFAULT_DECIMALS),
+    (function_with_kwargs, (), {"x": 3, "y": 4}, "FUNCTION_WITH_KWARGS(X=3, Y=4)", DEFAULT_DECIMALS),
+    (function_with_args_and_kwargs, (1, 2), {"x": 3, "y": 4}, "FUNCTION_WITH_ARGS_AND_KWARGS(A=1, B=2, X=3, Y=4)", DEFAULT_DECIMALS),
+    (function_with_args_kwargs_and_custom_thread, (1, 2), {"x": 3, "y": 4}, CUSTOM_THREAD, DEFAULT_DECIMALS),
+    (function_with_args_kwargs_custom_thread_and_custom_decimals, (1, 2), {"x": 3, "y": 4}, CUSTOM_THREAD, CUSTOM_DECIMALS),
 ])
-def test_function_timer_decorator_for_function_with_arguments(function: Callable[..., None], args: tuple[Any, ...], kwargs: dict[str, Any], expected_thread_name: str, capfd: object) -> None:
+def test_function_timer_decorator_for_function_with_arguments(function: Callable[..., None], args: tuple[Any, ...], kwargs: dict[str, Any], expected_thread_name: str, decimals: int, capfd: object) -> None:
     ensure_all_timer_threads_are_stopped()
     function(*args, **kwargs)
     terminal_output, _ = capfd.readouterr()
-    expected_output_regex = successful_output_regex(thread=expected_thread_name)
+    expected_output_regex = successful_output_regex(thread=expected_thread_name, decimals=decimals)
     assert re.fullmatch(expected_output_regex, terminal_output)
