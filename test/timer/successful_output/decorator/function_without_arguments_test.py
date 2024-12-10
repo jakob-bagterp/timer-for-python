e83322e5ca352e01b8080695606ce8cf6a9e55a4
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-10 20:06:30 +0100

    Refactored tests of function decorator to variation without arguments.

--- test/timer/successful_output/decorator/function_without_arguments_test.py
+++ test/timer/successful_output/decorator/function_without_arguments_test.py
@@ -4,31 +4,33 @@ from collections.abc import Callable
 
 import pytest
 from _helper.terminal_output import successful_output_regex
+from _helper.timer import ensure_all_timer_threads_are_stopped
 
 from timer.decorator.function import function_timer
 
 TEST_THREAD = "test"
 TEST_DECIMALS = 5
+ULTRA_SHORT_DELAY = 0.001
 
 
 @function_timer()
-def function_to_be_timed(seconds: float) -> None:
-    time.sleep(seconds)
+def function_to_be_timed() -> None:
+    time.sleep(ULTRA_SHORT_DELAY)
 
 
 @function_timer(thread=TEST_THREAD)
-def function_to_be_timed_with_custom_thread(seconds: float) -> None:
-    time.sleep(seconds)
+def function_to_be_timed_with_custom_thread() -> None:
+    time.sleep(ULTRA_SHORT_DELAY)
 
 
 @function_timer(decimals=TEST_DECIMALS)
-def function_to_be_timed_with_custom_decimals(seconds: float) -> None:
-    time.sleep(seconds)
+def function_to_be_timed_with_custom_decimals() -> None:
+    time.sleep(ULTRA_SHORT_DELAY)
 
 
 @function_timer(thread=TEST_THREAD, decimals=TEST_DECIMALS)
-def function_to_be_timed_with_custom_thread_and_decimals(seconds: float) -> None:
-    time.sleep(seconds)
+def function_to_be_timed_with_custom_thread_and_decimals() -> None:
+    time.sleep(ULTRA_SHORT_DELAY)
 
 
 @pytest.mark.parametrize("function, thread, decimals", [
@@ -37,8 +39,9 @@ def function_to_be_timed_with_custom_thread_and_decimals(seconds: float) -> None
     (function_to_be_timed_with_custom_decimals, None, TEST_DECIMALS),
     (function_to_be_timed_with_custom_thread_and_decimals, TEST_THREAD, TEST_DECIMALS),
 ])
-def test_function_timer_decorator(function: Callable[[float], None], thread: str | None, decimals: int | None, capfd: object) -> None:
-    _ = function(seconds=0.001)
+def test_function_timer_decorator_for_function_without_arguments(function: Callable[..., None], thread: str | None, decimals: int | None, capfd: object) -> None:
+    ensure_all_timer_threads_are_stopped()
+    function()
     terminal_output, _ = capfd.readouterr()
     thread_name = thread if thread is not None else function.__name__
     expected_output_regex = successful_output_regex(thread_name, decimals) if decimals is not None else successful_output_regex(thread_name)
