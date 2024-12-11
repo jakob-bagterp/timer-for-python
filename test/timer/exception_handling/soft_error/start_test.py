dc31ed2cbf4197e8581122457d96df760e167701
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-11 22:14:38 +0100

    Following successful test run on GitHug Actions, another attempted to fix the line separator issue and remove any skipped tests.

--- test/timer/exception_handling/soft_error/start_test.py
+++ test/timer/exception_handling/soft_error/start_test.py
@@ -1,7 +1,4 @@
-from os import linesep
-
-import pytest
-from _helper import operating_system
+from _constant.terminal import LINESEP
 from _helper.random import random_thread_name
 from _helper.timer import ensure_all_timer_threads_are_stopped
 from colorist import Color
@@ -19,11 +16,6 @@ def test_timer_start_thread_name_collision_soft_error_without_context_manager(ca
 
 
 def test_timer_start_thread_name_collision_soft_error_with_context_manager(capfd: object) -> None:
-    if operating_system.is_windows():
-        pytest.skip("Skipping test for Windows due to line separator issue.")  # pragma: no cover
-        # TODO: Fix line separator issue on Windows.
-        return  # pragma: no cover
-
     custom_thread = random_thread_name()
     timer = ensure_all_timer_threads_are_stopped()
     timer.start(thread=custom_thread)
@@ -31,7 +23,7 @@ def test_timer_start_thread_name_collision_soft_error_with_context_manager(capfd
         pass
     terminal_output, _ = capfd.readouterr()
     assert str(terminal_output).startswith(
-        f"{Color.YELLOW}Timer for thread {custom_thread.upper()} is running. Use .stop(thread='{custom_thread.upper()}') to stop it.{Color.OFF}{linesep}" + "Elapsed time:")
+        f"{Color.YELLOW}Timer for thread {custom_thread.upper()} is running. Use .stop(thread='{custom_thread.upper()}') to stop it.{Color.OFF}{LINESEP}" + "Elapsed time:")
     assert str(terminal_output).endswith(f" for thread {Color.GREEN}{custom_thread.upper()}{Color.OFF}\n")
 
 
