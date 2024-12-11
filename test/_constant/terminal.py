ea234531bcbfd46776a35084e5d51a57380b38fd
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-11 21:56:42 +0100

    Another attempt to fix issue based on logs.

--- test/_constant/terminal.py
+++ test/_constant/terminal.py
@@ -2,4 +2,4 @@ from os import linesep
 
 from _helper import operating_system
 
-LINESEP = "\r\n" if operating_system.is_windows() else linesep
+LINESEP = "\n" if operating_system.is_windows() else linesep
