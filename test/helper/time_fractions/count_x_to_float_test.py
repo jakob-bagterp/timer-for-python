2da3139ef405bb35dd654bb509efcbc899f1eb66
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-12 08:05:47 +0100

    Attempt to fix flaky tests with math.isclose() method instead of direct equality check.

--- test/helper/time_fractions/count_x_to_float_test.py
+++ test/helper/time_fractions/count_x_to_float_test.py
@@ -1,3 +1,5 @@
+import math
+
 from _helper.time_fractions import (random_microseconds_as_ns,
                                     random_milliseconds_as_ns,
                                     random_seconds_as_ns)
@@ -12,7 +14,7 @@ def test_count_microseconds_to_float() -> None:
         rounded_microseconds_to_float = round(TimeFractions(
             mock_elapsed_time_ns).count_microseconds_to_float(), float_precision)
         rounded_mock_microseconds_to_float = round(mock_elapsed_time_ns / 1000, float_precision)
-        assert rounded_microseconds_to_float == rounded_mock_microseconds_to_float
+        assert math.isclose(rounded_microseconds_to_float, rounded_mock_microseconds_to_float, rel_tol=1e-6)
 
 
 def test_count_milliseconds_to_float() -> None:
@@ -22,7 +24,7 @@ def test_count_milliseconds_to_float() -> None:
         rounded_milliseconds_to_float = round(TimeFractions(
             mock_elapsed_time_ns).count_milliseconds_to_float(), float_precision)
         rounded_mock_milliseconds_to_float = round(mock_elapsed_time_ns / 1000 / 1000, float_precision)
-        assert rounded_milliseconds_to_float == rounded_mock_milliseconds_to_float
+        assert math.isclose(rounded_milliseconds_to_float, rounded_mock_milliseconds_to_float, rel_tol=1e-9)
 
 
 def test_count_seconds_to_float() -> None:
@@ -31,4 +33,4 @@ def test_count_seconds_to_float() -> None:
         mock_elapsed_time_ns = random_seconds_as_ns()
         rounded_seconds_to_float = round(TimeFractions(mock_elapsed_time_ns).count_seconds_to_float(), float_precision)
         rounded_mock_seconds_to_float = round(mock_elapsed_time_ns / 1000 / 1000 / 1000, float_precision)
-        assert rounded_seconds_to_float == rounded_mock_seconds_to_float
+        assert math.isclose(rounded_seconds_to_float, rounded_mock_seconds_to_float, rel_tol=1e-12)
