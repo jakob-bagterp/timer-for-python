fcea0061a203efd6768b64b5dc00867be0ad0558
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-10-15 18:25:23 +0200

    Attempted fix of test issue by removing NumPy as dependency and refactoring tests.

--- test/helper/decimals/validate_and_normalise_test.py
+++ test/helper/decimals/validate_and_normalise_test.py
@@ -1,4 +1,3 @@
-import numpy
 from _mock_data.decimals import DECIMALS_RANGE
 
 from timer.constant.decimals import DEFAULT, MAXIMUM, MINIMUM
@@ -21,17 +20,17 @@ def test_negative_number_should_default() -> None:
 
 
 def test_float_within_minimum_to_maximum_should_not_default() -> None:
-    for decimals in numpy.arange(MINIMUM, MAXIMUM + 0.4, 0.1):
+    for decimals in [MINIMUM + 0.1 * i for i in range(int((MAXIMUM + 0.4 - MINIMUM) / 0.1))]:
         assert validate_and_normalise(decimals) == int(round(decimals))
 
 
 def test_float_larger_than_maximum_should_default() -> None:
-    for decimals in numpy.arange(MAXIMUM + 0.5, 100, 0.1):
+    for decimals in [MAXIMUM + 0.5 + 0.1 * i for i in range(int((100 - (MAXIMUM + 0.5)) / 0.1))]:
         assert validate_and_normalise(decimals) == MAXIMUM
 
 
 def test_negative_float_should_default() -> None:
-    for decimals in numpy.arange(-100, 0, 0.1):
+    for decimals in [x / 10.0 for x in range(-1000, 0)]:
         assert validate_and_normalise(decimals) == DEFAULT
 
 
