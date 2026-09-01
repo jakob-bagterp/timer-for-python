311288ef9de8d89252b8952d5b74ed2c87a42828
Jakob Bagterp <jakob_bagterp@hotmail.com>
2026-09-01 15:10:57 +0200

    Skip test_function_timer_decorator_for_function_without_arguments() for Windows 3.10 due to instabilities: "FAILED test/timer/successful_output/decorator/function_without_arguments_test.py::test_function_timer_decorator_for_function_without_arguments[function_to_be_timed_with_custom_thread_and_decimals-custom-5] - AssertionError: assert None    +  where None = <function fullmatch at 0x000001836FA66830>('Elapsed time: \\d+\\.\\d{5} milliseconds\\ for\\ thread\\ \x1b\\[32mCUSTOM\x1b\\[0m\\n', 'Elapsed time: 272.20000 microseconds for thread \x1b[32mCUSTOM\x1b[0m\n')"

--- test/timer/successful_output/decorator/function_without_arguments_test.py
+++ test/timer/successful_output/decorator/function_without_arguments_test.py
@@ -42,6 +42,10 @@ def function_to_be_timed_with_custom_thread_and_decimals() -> None:
         (function_to_be_timed_with_custom_thread_and_decimals, CUSTOM_THREAD, CUSTOM_DECIMALS),
     ],
 )
+@pytest.mark.skipif(
+    operating_system.is_windows() and python_version.is_3_10(),
+    reason="Skipping test for Python 3.10 on Windows since the sleep timer is flaky and inaccurate.",
+)
 def test_function_timer_decorator_for_function_without_arguments(
     function: Callable[..., None], thread: str | None, decimals: int | None, capfd: object
 ) -> None:
