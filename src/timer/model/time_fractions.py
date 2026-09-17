0f4d785fd6664431f54616dd0d7c3403aac72f20
Jakob Bagterp <jakob_bagterp@hotmail.com>
2026-09-17 16:43:05 +0200

    Updated descriptions of properties in TimeFractions class.

--- src/timer/model/time_fractions.py
+++ src/timer/model/time_fractions.py
@@ -11,26 +11,30 @@ class TimeFractions:
 
     @property
     def total_microseconds(self) -> float:
+        """Count the total microseconds and anything below that."""
+
         return self.time.microseconds + self.time.nanoseconds / 1_000
 
     @property
     def total_milliseconds(self) -> float:
-        """This could potentially be faster by dividing self.microseconds by a 1,000 directly, yet we don't want to lose precision in the decimals."""
+        """Count the total milliseconds and anything below that. This could potentially be faster by dividing self.microseconds by a 1,000 directly, yet we don't want to lose precision in the decimals."""
 
         return self.time.milliseconds + self.total_microseconds / 1_000
 
     @property
     def total_seconds(self) -> float:
-        """This could potentially be faster by dividing self.milliseconds by a 1,000 directly, yet we don't want to lose precision in the decimals."""
+        """Count the total seconds and anything below that. This could potentially be faster by dividing self.milliseconds by a 1,000 directly, yet we don't want to lose precision in the decimals."""
 
         return self.time.seconds + self.total_milliseconds / 1_000
 
     @property
     def total_seconds_rounded(self) -> float:
-        """For instance, if 2 seconds and 567 milliseconds, ensure it'll be rounded up to 3 seconds."""
+        """Count the total seconds and anything below that. For instance, if 2 seconds and 567 milliseconds, ensure it'll be rounded up to 3 seconds."""
 
         return int(round(self.total_seconds, 0))
 
     @property
     def total_minutes_as_seconds(self) -> float:
+        """Count the total minutes as seconds and anything below that."""
+
         return self.time.minutes * SECONDS_PER_MINUTE + self.total_seconds
